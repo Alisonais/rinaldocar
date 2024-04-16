0045eb6233725e70bab68fdee2efdfdc18c1b4f3
Alisonais <alisonais@ymail.com>
2024-04-16 11:56:18 -0300

    first commit

---
+++ assets/js/slider.js
@@ -0,0 +1,14 @@
+var radio = document.querySelector('.manualbtn');
+var cont = 0;
+
+setInterval(() => {
+  nextImg();
+}, 5000);
+
+function nextImg() {
+  cont++
+
+  cont > 4 ? cont = 1: cont;
+
+  document.getElementById(`radio${cont}`).checked = true;
+}
